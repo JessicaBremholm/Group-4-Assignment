db7e7c0b7a7fad63e6d3565b6bea695dd0ac71c6
Mark <markmiasnikov@gmail.com>
2022-11-16 14:10:05 +0100

    Added JS for hover effect
    And other things like classes etc

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+var images = document.querySelectorAll('.portrait__img')
+for (var i = 0; i < images.length; i ++) {
+    images[i].style.transitionDuration = '0.1s'
+    images[i].nextElementSibling.style.transitionDuration = '0.1s'
+    images[i].nextElementSibling.style.opacity = '0'
+    function mousecolor() {
+        console.log("det funkar");
+        this.style.filter = "grayscale(0%)";
+        
+    
+  }
+
+    function mousegrey() {
+
+    this.style.filter = "grayscale(100%)";
+
+  }
+
+  images[i].addEventListener('mouseover', mousecolor); 
+  images[i].addEventListener('mouseout', mousegrey); 
+}
